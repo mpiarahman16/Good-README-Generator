cc8984bbecc5356605ee46e3ce299fbb7b07f658
mpiarahman16 <mpiarahman@gmail.com>
2020-04-10 19:52:06 -0400

    adding hw, need to add 2 more functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,61 @@
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+
 const questions = [
+    {
+        type: "input",
+        name: "username",
+        message: "What is your github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },
+    {
+        type: "input",
+        name: "projecturl",
+        message: "What is your github project url?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project name?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a shoort description of your project."
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Does your project require or have a license?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What command should be run to install dependencies?"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run test?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What does a user need to know to use this project?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "What does a user need to know about contributing to this project?"
+    },
+
+
 
 ];
 
@@ -10,3 +67,49 @@ function init() {
 }
 
 init();
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+
+
+
+
+function generateHTML(answers) {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+    <p class="lead">I am from ${answers.location}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${answers.github}</li>
+      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+}
+
+promptUser()
+    .then(function (answers) {
+        const html = generateHTML(answers);
+
+        return writeFileAsync("index.html", html);
+    })
+    .then(function () {
+        console.log("Successfully wrote to index.html");
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
